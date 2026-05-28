bba3f72ed65943e072fef16f492b19feb21bd2a9
Matheus Arthur <matheus.artsilva@gmail.com>
2026-05-28 13:12:43 -0300

    feat(p2): adiciona a busca por nome de um pais

--- p2-paises/App.tsx
+++ p2-paises/App.tsx
@@ -10,15 +10,20 @@ import {
 export default function App() {
 
   const [nome, setNome] = useState('')
-  const [resultado, setResultado] = useState(null)
+  const [nomeComum, setNomeComum] = useState('')
+  const [nomeOficial, setNomeOficial] = useState('')
+  const [traduzidoRusso, setTraduzidoRusso] = useState('')
+  const [foto, setFoto] = useState('')
 
   const procuraNome = async () => {
-    const resposta = await fetch(`https://restcountries.com/v3.1/name/${nome}`);
+    const resposta = await fetch(`https://restcountries.com/v3.1/name/${nome}`)
 
-    const dados = await resposta.json();
-      setResultado(dados[0]);
-      console.log(dados[0]);
-  };
+    const dados = await resposta.json()
+    setNomeComum(dados[0].name.common)
+    setNomeOficial(dados[0].name.official)
+    setTraduzidoRusso(dados[0].translations.rus.common)
+    setFoto(dados[0].maps.openStreetMaps)
+  }
 
   return (
     <View style={styles.container}>
@@ -30,8 +35,17 @@ export default function App() {
       onChangeText={setNome}
       />
       <Pressable style={styles.button} onPress={procuraNome}>
-          <Text style={styles.buttonText}>PROCURAR</Text>
+          <Text style={styles.buttonText}>PROCURAR PAÍS</Text>
       </Pressable>
+      <View style={styles.result}>
+          <Text style={styles.title}>RESULTADO DA PROCURA</Text>
+          <View style={styles.resultDiv}>
+            <Text style={styles.label}>Nome comum: {nomeComum}</Text>
+            <Text style={styles.label}>Nome oficial: {nomeOficial}</Text>
+            <Text style={styles.label}>Nome em russo: {traduzidoRusso}</Text>
+            <Text style={styles.label}>Foto do país: {foto}</Text>
+          </View>
+      </View>
     </View>
   );
 }
@@ -66,6 +80,26 @@ const styles = StyleSheet.create({
     padding: 12,
     textAlign: 'center',
     width: '80%'
+  },
+  label: {
+    color: 'white',
+    fontSize: 15,
+    fontWeight: 'bold',
+    marginBottom: 6
+  },
+  result:{
+    alignItems: 'center',
+    marginTop: 8,
+    paddingVertical: 24,
+    width: '80%'
+  },
+  resultDiv: {
+    backgroundColor: '#0F172A',
+    borderColor: 'gray',
+    borderRadius: 4,
+    borderWidth: 1,
+    padding: 16,
+    width: '80%',
   },
   title: {
     color: 'white',
