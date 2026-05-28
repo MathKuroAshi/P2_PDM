47a9914198c72a7f26f588583aad9c737c35cfc6
Matheus Arthur <matheus.artsilva@gmail.com>
2026-05-28 11:26:58 -0300

    feat(p2): obtem dados da api restcountries

--- p2-paises/App.tsx
+++ p2-paises/App.tsx
@@ -1,20 +1,77 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import {
+  Pressable,
+  StyleSheet, 
+  Text, 
+  TextInput, 
+  View 
+} from 'react-native';
 
 export default function App() {
+
+  const [nome, setNome] = useState('')
+  const [resultado, setResultado] = useState(null)
+
+  const procuraNome = async () => {
+    const resposta = await fetch(`https://restcountries.com/v3.1/name/${nome}`);
+
+    const dados = await resposta.json();
+      setResultado(dados[0]);
+      console.log(dados[0]);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>PROCURAR PAÍSES/CAPITAIS</Text>
+      <TextInput
+      style={styles.input}
+      placeholder="Digite o nome do local"
+      value={nome}
+      onChangeText={setNome}
+      />
+      <Pressable style={styles.button} onPress={procuraNome}>
+          <Text style={styles.buttonText}>PROCURAR</Text>
+      </Pressable>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    button: {
     alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#64748B',
+    borderRadius: 4,
+    marginTop: 8,
+    padding: 12,
+    width: '80%',
   },
+  buttonText: {
+    color: 'white',
+    fontSize: 14,
+    fontWeight: 'bold'
+  },
+  container: {
+    alignItems: 'center',
+    backgroundColor: '#0F172A',
+    flex: 1,
+    justifyContent: 'center',
+    padding: 24,
+  },
+  input: {
+    borderColor: 'gray',
+    borderRadius: 4,
+    borderWidth: 1,
+    color: 'white',
+    marginBottom: 12,
+    padding: 12,
+    textAlign: 'center',
+    width: '80%'
+  },
+  title: {
+    color: 'white',
+    fontSize: 25,
+    fontWeight: 'bold',
+    marginBottom: 24,
+    textAlign: 'center',
+  }
 });
